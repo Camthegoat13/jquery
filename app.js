a994845f76d1743152570c7e618cbbec88530cb2
camthegoat13 <aubreybaby@gmail.com>
2021-09-30 19:19:49 -0600

    added to the staging area

---
+++ app.js
@@ -0,0 +1,9 @@
+let start = $('.navItem').css('color', 'white');    
+
+let id = $('#nav').css('background-color', '#145');
+
+let list = $('li:even').css('background-color', '#ccc')
+
+let odd = $('li:odd').css('background-color', '#aaa')
+
+let name = $('#myInput').val('Cameron')
